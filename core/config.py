08425f8ac4c9ad8723ab8902942fdac6033b8d8c
captaintwin <dmvl.rybakov@gmail.com>
2025-10-27 14:58:26 +0100

    itnews_portal

--- core/config.py
+++ core/config.py
@@ -1,8 +1,6 @@
 import os
 from dotenv import load_dotenv
 load_dotenv()
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-MODEL_NAME = os.getenv("MODEL_NAME")
 
 #Telegram
 BOT_TOKEN = os.getenv("BOT_TOKEN")
